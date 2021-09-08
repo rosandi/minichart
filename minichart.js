a78ec53899cb689aaf4108a25c08e27d62e9d148
Yudi Rosandi <rosandi@gmail.com>
2021-09-08 09:37:35 +0700

    barchart added

--- minichart.js
+++ minichart.js
@@ -3,25 +3,24 @@
 colors = ["#E74C3C","#9B59B6","#5499C7","#52BE80","#F4D03F","#F4D03F"];
 
 // data format:
-function piechart(cvs, data, name=null) {
+function piechart(cvs, data, name=null, vertical=false) {
     var canvas = document.getElementById(cvs);
     var ctx = canvas.getContext('2d');
     var w = canvas.width;
     var h = canvas.height;
+    var r = w/2 -30;
     var cx = w/2;
-    var cy = h/2;
+    var cy = r+30;
     w -= 30;
     h -= 30;
-    cy-= 30;
     
     var total=0;
     var pct=new Array(data.length);
     var onec=2*Math.PI;
-    var r = Math.min(w,h)/2;
     ctx.font = "20px Georgia";
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.shadowBlur = 10;
     ctx.shadowColor = "black";
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
     
     for(i=0;i<data.length;i++) total+=data[i];
     for(i=0;i<data.length;i++) pct[i]=data[i]/total;
@@ -39,24 +38,101 @@ function piechart(cvs, data, name=null) {
         
         ctx.fillStyle = 'yellow';
         th=(st+ang)/2;
-        px=cx+r*Math.cos(th)/2;
-        py=cy+r*Math.sin(th)/2;
+        px=cx+(r+30)*Math.cos(th)/2-15;
+        py=cy+(r+30)*Math.sin(th)/2+8;
         ctx.fillText(((pct[i])*100).toFixed(0)+"%",px,py);
         st+=pct[i]*onec;
     }
+    
+    // put a nice belly button
+    ctx.beginPath();
+    ctx.arc(cx,cy,20,0,2*Math.PI)
+    ctx.fillStyle = 'white';
+    ctx.fill();
     
     if(name == null) return;
     if(name.length != data.length) return;
     
     var bln=canvas.width/data.length;
     var yo = cy+r+30;
-    var xo=0;
     
+    if (vertical) {
+        var xo=30;
+        for(i=0;i<data.length;i++) {
+            ctx.fillStyle = colors[i];
+            ctx.fillRect(xo+10, yo, 25, 25);
+            ctx.fillText(name[i], xo+40, yo+20);
+            yo+=30;
+        }
+    } else {
+        var xo=0;
+        for(i=0;i<data.length;i++) {
+            ctx.fillStyle = colors[i];
+            ctx.fillRect(xo+10, yo, 25, 25);
+            ctx.fillText(name[i], xo+40, yo+20);
+            xo+=bln;
+        }
+    }
+}
+
+function barchart(cvs, data, name=null, vertical=false, barh=30) {
+    var canvas = document.getElementById(cvs);
+    var ctx = canvas.getContext('2d');
+    var w = canvas.width;
+    var h = canvas.height;
+    ofx= 10;
+    ofy= 10;
+    
+    w -= ofx*2;
+    h -= ofy*2;
+    
+    var total=0;
+    var pct=new Array(data.length);
+    ctx.font = "20px Georgia";
+    
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    
+    ctx.shadowBlur = 10;
+    ctx.shadowColor = "black";
+    
+    for(i=0;i<data.length;i++) total+=data[i];
+    for(i=0;i<data.length;i++) pct[i]=data[i]/total;
+    
+    var st=0;
     for(i=0;i<data.length;i++) {
+        dis=pct[i]*w;
+        ctx.beginPath();
+        ctx.rect(st+ofx,ofy,dis,barh)
         ctx.fillStyle = colors[i];
-        ctx.fillRect(xo+10, yo, 25, 25);
-        ctx.fillText(name[i], xo+40, yo+20);
-        xo+=bln;
+        ctx.fill();
+        
+        ctx.fillStyle = 'yellow';
+        
+        ctx.fillText(((pct[i])*100).toFixed(0)+"%",ofx+st+10,barh);
+        st+=pct[i]*w;
+    }
+    
+    if(name == null) return;
+    if(name.length != data.length) return;
+    
+    var bln=canvas.width/data.length;
+    var yo = barh+2*ofy;
+    var xo=ofx;
+    
+    if (vertical) {
+        for(i=0;i<data.length;i++) {
+            ctx.fillStyle = colors[i];
+            ctx.fillRect(xo+10, yo, 25, 25);
+            ctx.fillText(name[i], xo+40, yo+20);
+            yo+=30;
+        }
+    } else {
+        for(i=0;i<data.length;i++) {
+            ctx.fillStyle = colors[i];
+            ctx.fillRect(xo+10, yo, 25, 25);
+            ctx.fillText(name[i], xo+40, yo+20);
+            xo+=bln;
+        }
     }
     
 }
